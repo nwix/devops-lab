0423e5c74e74872f61477e257b4786defff9c018
ILYAS <atbirilyas10@gmail.com>
2026-04-23 15:25:10 +0100

    mo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/") 
 def home():
     # Pour la Partie 7, changez ce texte par "Version 2 Déployée automatiquement" [cite: 161, 162]
-    return "Version 1-1    hfhhf CI/CD avec Jenkins" 
+    return "Version 1 CI/CD avec Jenkins" 
 
 if __name__ == "__main__": 
     app.run(host="0.0.0.0", port=5000) 
